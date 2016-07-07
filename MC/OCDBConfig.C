36f39239838c7a7d91191b2d0dd93418087f643d
Roberto Preghenella <preghenella@bo.infn.it>
2016-07-07 14:14:22 +0200

    Back to single OCDB configuration function, with OCDB objects set according to TPC recommendations. https://twiki.cern.ch/twiki/bin/view/ALICE/TPC_Offline_simrecSettings_FAQ

--- MC/OCDBConfig.C
+++ MC/OCDBConfig.C
@@ -53,18 +53,14 @@ OCDBDefault(Int_t run, Int_t mode)
   man->SetDefaultStorage(Form("alien://Folder=/alice/data/%d/OCDB", year));
   man->SetRun(run);
   
-  //
   // set detector specific paths according to LHC Run
-  if (year < 2015)
-    DefaultSpecificStorageRun1(man, run, mode);
-  else
-    DefaultSpecificStorageRun2(man, run, mode);
+  DefaultSpecificStorageRun(man, run, mode);
 
 }
 
 /*****************************************************************/
 
-DefaultSpecificStorageRun1(AliCDBManager *man, Int_t run, Int_t mode)
+DefaultSpecificStorageRun(AliCDBManager *man, Int_t run, Int_t mode)
 {
   
   gROOT->LoadMacro("$ALIDPG_ROOT/MC/Utils.C");
@@ -77,7 +73,7 @@ DefaultSpecificStorageRun1(AliCDBManager *man, Int_t run, Int_t mode)
   // for AD hack 
   const Char_t *Raw2015  = "alien://Folder=/alice/data/2015/OCDB";
   
-  // DEFAULT RUN-1 SPECIFIC OBJECTS 
+  // DEFAULT SPECIFIC OBJECTS 
   const Char_t *SpecificStorageList[][3] = {
     // path                    sim       rec
     //
@@ -85,77 +81,32 @@ DefaultSpecificStorageRun1(AliCDBManager *man, Int_t run, Int_t mode)
     "ITS/Align/Data",          Ideal,    Residual, // ok
     "ITS/Calib/SPDSparseDead", Full,     Residual, // ok ?
     // TPC
-    "TPC/Calib/Parameters",    Residual, Residual, // do we need this ?
-    "TPC/Calib/ClusterParam",  Residual, Residual, // Ideal, Residual ?
-    "TPC/Calib/RecoParam",     Residual, Residual, // ok ?
-    "TPC/Calib/TimeGain",      Ideal,    Residual, // ok
-    "TPC/Calib/Correction",    Ideal,    Residual, // ok
-    "TPC/Align/Data",          Ideal,    Residual, // ok
-    "TPC/Calib/TimeDrift",     Ideal,    Residual, // ok
-    "TPC/Calib/AltroConfig",   Ideal,    Residual, // do we need this ?
+    "TPC/Calib/ClusterParam",  Ideal,    Residual, // ok!
+    "TPC/Calib/RecoParam",     Residual, Residual, // ok!
+    "TPC/Calib/TimeGain",      Ideal,    Residual, // ok!
+    "TPC/Calib/Correction",    Ideal,    Residual, // ok!
+    "TPC/Align/Data",          Ideal,    Residual, // ok!
+    "TPC/Calib/TimeDrift",     Ideal,    Residual, // ok!
     // MUON
     "MUON/Align/Data",         Ideal,    Residual, // ok
     // ZDC
     "ZDC/Align/Data",          Ideal,    Ideal,    // ok ?
     "ZDC/Calib/Pedestals",     Ideal,    Ideal,    // do we need this ?
-    // AD
-    "AD/Calib/QAParam",        Raw2015,  Raw2015   // temporary HACK
   };
   const Int_t nSpecificStorages = sizeof(SpecificStorageList) / (3 * sizeof(Char_t *));
 
   // set specific storages
   for (Int_t isto = 0; isto < nSpecificStorages; isto++) {
-    if (SpecificStorageList[mode][isto]) {
+    if (SpecificStorageList[mode+1][isto]) {
       printf("Setting specific storage: %s -> %s\n", SpecificStorageList[isto][0], SpecificStorageList[isto][mode+1]);
       man->SetSpecificStorage(SpecificStorageList[isto][0], SpecificStorageList[isto][mode+1]);
     }
   }
 
-}
-
-/*****************************************************************/
-
-DefaultSpecificStorageRun2(AliCDBManager *man, Int_t run, Int_t mode)
-{
+  // temporary hacks
+  if (year < 2015)
+    man->SetSpecificStorage("AD/Calib/QAParam", Raw2015);
+  if (year > 2015)
+    man->SetSpecificStorage("TPC/Calib/RecoParam", Full);
   
-  gROOT->LoadMacro("$ALIDPG_ROOT/MC/Utils.C");
-  Int_t year = RunToYear(run);
-
-  const Char_t *Raw      = Form("alien://Folder=/alice/data/%d/OCDB", year);
-  const Char_t *Ideal    = "alien://Folder=/alice/simulation/2008/v4-15-Release/Ideal/";
-  const Char_t *Residual = "alien://Folder=/alice/simulation/2008/v4-15-Release/Residual/";
-  const Char_t *Full     = "alien://Folder=/alice/simulation/2008/v4-15-Release/Full/";
-  
-  // DEFAULT RUN-1 SPECIFIC OBJECTS 
-  const Char_t *SpecificStorageList[][3] = {
-    // path                    sim       rec
-    //
-    // ITS
-    "ITS/Align/Data",          Ideal,    Residual,
-    "ITS/Calib/SPDSparseDead", Full,     Residual,
-    // TPC
-    "TPC/Calib/Parameters",    Residual, Residual,
-    "TPC/Calib/ClusterParam",  Residual, Residual,
-    "TPC/Calib/RecoParam",     Full,     Full,      // this needs to be moved to Residual
-    "TPC/Calib/TimeGain",      Ideal,    Residual,
-    "TPC/Calib/Correction",    Ideal,    Residual,
-    "TPC/Align/Data",          Ideal,    Residual,
-    "TPC/Calib/TimeDrift",     Ideal,    Residual,
-    // MUON
-    "MUON/Align/Data",         Ideal,    Residual,
-    // ZDC
-    "ZDC/Align/Data",          Ideal,    Ideal,
-    "ZDC/Calib/Pedestals",     Ideal,    Ideal
-  };
-  const Int_t nSpecificStorages = sizeof(SpecificStorageList) / (3 * sizeof(Char_t *));
-
-  // set specific storages
-  for (Int_t isto = 0; isto < nSpecificStorages; isto++) {
-    if (SpecificStorageList[mode][isto]) {
-      printf("Setting specific storage: %s -> %s\n", SpecificStorageList[isto][0], SpecificStorageList[isto][mode+1]);
-      man->SetSpecificStorage(SpecificStorageList[isto][0], SpecificStorageList[isto][mode+1]);
-    }
-  }
-
 }
-
