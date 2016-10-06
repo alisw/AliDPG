ca2e5f02e5543c98b3a5f5dbcf59a8170802c32c
Roberto Preghenella <preghenella@bo.infn.it>
2016-10-06 13:04:29 +0200

    Removed ZDC/Calib/Pedestals from SpecificStorage (Chiara O)

--- MC/OCDBConfig.C
+++ MC/OCDBConfig.C
@@ -89,8 +89,7 @@ DefaultSpecificStorage(AliCDBManager *man, Int_t mode)
     // MUON
     "MUON/Align/Data",         Full,     NULL,     // ok!
     // ZDC
-    "ZDC/Align/Data",          Ideal,    Ideal,    // ok ?
-    "ZDC/Calib/Pedestals",     Ideal,    Ideal,    // do we need this ?
+    "ZDC/Align/Data",          Ideal,    Ideal     // ok
   };
   const Int_t nSpecificStorages = sizeof(SpecificStorageList) / (3 * sizeof(Char_t *));
 
