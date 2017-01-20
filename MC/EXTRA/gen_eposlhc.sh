32812908cb9e25042f546d5a9a9837600b7890df
Roberto Preghenella <preghenella@bo.infn.it>
2017-01-20 08:10:28 +0100

    Updated CRMC package version for EPOS LHC running

--- MC/EXTRA/gen_eposlhc.sh
+++ MC/EXTRA/gen_eposlhc.sh
@@ -11,7 +11,7 @@ echo "running in `pwd`, writing HepMC to $1"
 # prepare environment
 source /cvmfs/alice.cern.ch/etc/login.sh
 eval $(alienv printenv GCC-Toolchain/v4.9.3-7)
-eval $(alienv printenv CRMC::v1.5.4-2)
+eval $(alienv printenv CRMC::v1.5.4-5)
 
 # force path to requested output
 sed -e s#__CRMC_BASEDIR__#"$CRMC_ROOT"# ${ALICE_PHYSICS}/PWG/MCLEGO/CRMC/crmc_template.param > crmc.param
