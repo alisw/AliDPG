79a276e0723e0524655795863ac45451a530004c
Valentina Zaccolo <valentina.zaccolo@to.infn.it>
2017-04-13 14:43:31 +0200

    Added diffraction tuned Pythia and Phojet to the Custom Generators

--- MC/CustomGenerators/PWGMM/Phojet_DiffTune.C
+++ MC/CustomGenerators/PWGMM/Phojet_DiffTune.C
@@ -6,13 +6,6 @@ GeneratorCustom()
 
   AliGenDPMjet* dpmjet = GeneratorPhojet(); 
       
-  dpmjet->SetMomentumRange(0, 999999.);
-  dpmjet->SetThetaRange(0., 180.);
-  dpmjet->SetYRange(-12.,12.);
-  dpmjet->SetPtRange(0,1000.);
-  dpmjet->SetProcess(kDpmMb);
-  dpmjet->SetEnergyCMS(energyConfig);
-  dpmjet->SetCrossingAngle(0,0.000280);      
   dpmjet->SetTuneForDiff();
 
   return dpmjet;
--- MC/CustomGenerators/PWGMM/Pythia_DiffTune.C
+++ MC/CustomGenerators/PWGMM/Pythia_DiffTune.C
@@ -5,15 +5,8 @@ GeneratorCustom()
 
       AliGenPythia *pythia = GeneratorPythia6(kPythia6Tune_Perugia0); 
       
-      pythia->SetMomentumRange(0, 999999.);
-      pythia->SetThetaRange(0., 180.);
-      pythia->SetYRange(-12.,12.);
-      pythia->SetPtRange(0,1000.);
-      pythia->SetProcess(kPyMb);
-      pythia->SetEnergyCMS(energyConfig);
       pythia->UseNewMultipleInteractionsScenario();
-      pythia->SetCrossingAngle(0,0.000280);
       pythia->SetTuneForDiff();
-//
+      
       return pythia;
 }
