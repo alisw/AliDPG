07fea49e0910b3d9d2a469cf0c392b564b8104ff
Roberto Preghenella <preghenella@bo.infn.it>
2017-04-19 14:32:04 +0200

    Custom generators based on DPMJET with triggered cascades (#19)

---
+++ MC/CustomGenerators/PWGLF/DPMJET_OmegaTrg_pPb8.C
@@ -0,0 +1,11 @@
+AliGenerator *GeneratorCustom(){
+
+  Int_t sign = uidConfig % 2 == 0 ? 1 : -1;
+  
+  AliGenDPMjet *dpmjet  = (AliGenDPMjet*) GeneratorPhojet();
+  
+  dpmjet->SetTriggerParticle(sign * 3334, 1.2);
+
+  return dpmjet;
+  
+}
