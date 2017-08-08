ff9c2daba112486652c17771d1e7428e6e385e79
Michael Weber <m.weber@cern.ch>
2017-08-08 17:14:18 +0200

    Bugfix: remove variable hfproc and check everything on opt

--- MC/CustomGenerators/PWGHF/EPOS_HFbjetpPb.C
+++ MC/CustomGenerators/PWGHF/EPOS_HFbjetpPb.C
@@ -1,5 +1,5 @@
 AliGenerator *
-GeneratorCustom(TString opt = "",TString hfproc="")
+GeneratorCustom(TString opt = "")
 {
   AliGenCocktail *ctl  = GeneratorCocktail("EPOS_HF");
   Float_t randHF = gRandom->Rndm();
@@ -10,22 +10,22 @@ GeneratorCustom(TString opt = "",TString hfproc="")
   //
   Int_t process[2] = {kPythia6HeavyProcess_Charm, kPythia6HeavyProcess_Beauty};
   Int_t decay[4]   = {kPythia6HeavyDecay_Hadrons, kPythia6HeavyDecay_HadronsWithV0, kPythia6HeavyDecay_Electron, kPythia6HeavyDecay_All};
-
+  
   const Char_t *label[2][4] = {
     "chadr PYTHIA", "chadr PYTHIA", "cele PYTHIA", "ccbar PYTHIA",
     "bchadr PYTHIA", "bchadr PYTHIA", "bele PYTHIA", "bbbar PYTHIA"
   };
-
+  
   Int_t iprocess = -1;
   
-  if(hfproc.Contains("cc")) {iprocess = 0; printf("-- HF process = %s -- \n",hfproc.Data());}
-  else if(hfproc.Contains("bb")) {iprocess = 1; printf("-- HF process = %s -- \n",hfproc.Data());}        
-  else printf("-- no HF process set = %s -- \n");
+  if(opt.Contains("cc")) {iprocess = 0; printf("-- HF process = %s -- \n",opt.Data());}
+  else if(opt.Contains("bb")) {iprocess = 1; printf("-- HF process = %s -- \n",opt.Data());}
+  else {printf("-- no HF process set = %s -- \n",opt.Data()); return NULL;};
   
   TString optList[5] = {"had", "hv0", "ele", "all", "jetjet"};
   Int_t idecay = 0;
   for (Int_t iopt = 0; iopt < 5; iopt++){
-    if (opt.EqualTo(optList[iopt]))
+    if (opt.Contains(optList[iopt]))
       idecay = iopt;
   }
   
@@ -51,3 +51,4 @@ GeneratorCustom(TString opt = "",TString hfproc="")
   }   
   return ctl;
 }
+
