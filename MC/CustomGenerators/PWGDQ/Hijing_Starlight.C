362dbbf9fda7be8678b2fdedf76ce5a3d9e1c05d
Ionut Arsene <iarsene@cern.ch>
2017-11-16 06:49:57 +0100

    Added custom generator for a cocktail of Hijing and Starlight; Small updates in GeneratorConfig::GeneratorStarlight() to accomodate the J/psi to dielectron and gamma+gamma->ee processes.

---
+++ MC/CustomGenerators/PWGDQ/Hijing_Starlight.C
@@ -0,0 +1,12 @@
+AliGenerator *
+GeneratorCustom()
+{
+  AliGenCocktail* cocktail = GeneratorCocktail("Hijing_Starlight");
+  AliGenerator* hijing = GeneratorHijing();
+  cocktail->AddGenerator(hijing, "Hijing", 1.);    
+  
+  AliGenerator* starlight = GeneratorStarlight();
+  cocktail->AddGenerator(starlight, "Starlight", 1.);
+  
+  return ctl;
+}
