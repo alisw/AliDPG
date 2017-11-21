8b9493f9bc260b138d4217f71a6db11ca2daeb2b
Ionut Arsene <iarsene@cern.ch>
2017-11-21 10:31:31 +0100

    small naming change in the macro which sets the Hijing + injected Jpsi simulation

--- MC/CustomGenerators/PWGDQ/Hijing_JpsieeXeXe.C
+++ MC/CustomGenerators/PWGDQ/Hijing_JpsieeXeXe.C
@@ -1,20 +1,20 @@
 AliGenerator *
 GeneratorCustom()
 {
-  AliGenCocktail *ctl   = GeneratorCocktail("Hijing_JpsieeXeXe");
+  AliGenCocktail *cocktail   = GeneratorCocktail("Hijing_JpsieeXeXe");
   AliGenerator   *hij   = GeneratorHijing();
-  ctl->AddGenerator(hij,  "Hijing", 1.);    
+  cocktail->AddGenerator(hij,  "Hijing", 1.);    
   if (uidConfig % 10 < 7) {
     AliGenerator *jpsi  = Generator_Jpsiee("PbPb 2.76", 1.0, 0.0, 0.0, 0.0);
-    ctl->AddGenerator(jpsi, "Jpsi2ee", 1., new TFormula("two", "2."));
+    cocktail->AddGenerator(jpsi, "Jpsi2ee", 1., new TFormula("two", "2."));
     TFile *file = new TFile("typeHF_4.proc", "recreate");
     file->Close();
   }
   else {
     AliGenerator *bjpsi = Generator_Jpsiee("Pythia BBar", 0.0, 0.0, 0.0, 1.0);
-    ctl->AddGenerator(bjpsi, "B2Jpsi2ee", 1., new TFormula("two", "2."));
+    cocktail->AddGenerator(bjpsi, "B2Jpsi2ee", 1., new TFormula("two", "2."));
     TFile *file = new TFile("typeHF_5.proc", "recreate");
     file->Close();
   }
-  return ctl;
+  return cocktail;
 }
