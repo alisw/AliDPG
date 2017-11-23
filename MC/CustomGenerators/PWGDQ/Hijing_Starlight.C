f7b07046d6c454bcbf18ce0eef1aaad6eda8d7ed
Ionut Arsene <iarsene@cern.ch>
2017-11-23 12:36:31 +0100

    Changed the order of adding the generators to the cocktail.

--- MC/CustomGenerators/PWGDQ/Hijing_Starlight.C
+++ MC/CustomGenerators/PWGDQ/Hijing_Starlight.C
@@ -2,11 +2,11 @@ AliGenerator *
 GeneratorCustom()
 {
   AliGenCocktail* cocktail = GeneratorCocktail("Hijing_Starlight");
-  AliGenerator* hijing = GeneratorHijing();
-  cocktail->AddGenerator(hijing, "Hijing", 1.);    
-  
   AliGenerator* starlight = GeneratorStarlight();
   cocktail->AddGenerator(starlight, "Starlight", 1.);
+  
+  AliGenerator* hijing = GeneratorHijing();
+  cocktail->AddGenerator(hijing, "Hijing", 1.);    
   
   return cocktail;
 }
