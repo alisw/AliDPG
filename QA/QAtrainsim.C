53ce9f9466f39f60745431492c2bc7bde23f4ac8
Roberto Preghenella <preghenella@bo.infn.it>
2017-12-19 11:04:37 +0100

    Revert "Removed manual support to embedding: automatic configuration of MC input handlers [ALIROOT-7434]"
    This reverts commit 8a9f434b1622359400b125c6a7c102391fad056f.

--- QA/QAtrainsim.C
+++ QA/QAtrainsim.C
@@ -202,6 +202,21 @@ void QAtrainsim(Int_t run = 0,
   mcHandler->SetPreReadMode(1);
   mcHandler->SetReadTR(kTRUE);
 
+  // subsidiary handler for mc-to-mc embedding
+  TString bgDir = gSystem->Getenv("CONFIG_BGEVDIR");
+  if (!bgDir.IsNull()) { // add extra handler for underlaying event
+    if (bgDir.BeginsWith("alien://") && !gGrid && !TGrid::Connect("alien://")) {
+      printf("Failed to create a grid connection\n");
+      abort();
+    }
+    if (!bgDir.EndsWith("/")) bgDir += "/";
+    AliMCEventHandler* mcHandlerBg = new AliMCEventHandler();
+    mcHandlerBg->SetInputPath(bgDir.Data());
+    mcHandlerBg->SetPreReadMode(1);
+    mcHandlerBg->SetReadTR(kTRUE);
+    mcHandler->AddSubsidiaryHandler(mcHandlerBg);
+  }
+  
   // AnalysisTasks
   //  mgr->Lock();
   mgr->SetFileInfoLog("fileinfo.log"); 
