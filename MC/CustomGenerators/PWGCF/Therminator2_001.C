d834db925de1365b23a17b4019d2602f57de2c43
Michael Weber <m.weber@cern.ch>
2018-02-07 10:49:22 +0100

    Remove old xml path

--- MC/CustomGenerators/PWGCF/Therminator2_001.C
+++ MC/CustomGenerators/PWGCF/Therminator2_001.C
@@ -3,9 +3,6 @@ AliGenerator * GeneratorCustom() {
   // ====================================================
   // set environment variables
 
-  // xml path for Pb-Pb at 2.76 TeV
-  gSystem->Setenv("THERM2_PARAMS_XML_PATH", "alien:/alice/cern.ch/user/m/mbuczyns/Therminator2_custom_hypersurface/lhyquid3v");
-
   // select 3+1 hydro model
   gSystem->Setenv("THERM2_PARAMS_FreezeOutModel", "Lhyquid3D");
 
