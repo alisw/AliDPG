2c8d32e686abc179c7fe8fb0b9935d70fe2938da
Livio Bianchi <Livio.Bianchi@cern.ch>
2018-02-08 12:45:17 +0100

    Custom generator for Omega injection into Hijing event (for Xe-Xe)

---
+++ MC/CustomGenerators/PWGLF/Hijing_XeXe_Omega.C
@@ -0,0 +1,12 @@
+AliGenerator *
+GeneratorCustom()
+{
+  AliGenCocktail *ctl  = GeneratorCocktail("Hijing_XeXe_Omega");
+  AliGenerator   *hij  = GeneratorHijing();
+  AliGenerator   *iom  = GeneratorInjector(1, 3334, 0.5, 7., -0.7, 0.7);
+  AliGenerator   *iaom = GeneratorInjector(1, -3334, 0.5, 7., -0.7, 0.7);
+  ctl->AddGenerator(hij,  "Hijing",           1.);
+  ctl->AddGenerator(iom,  "Injector (Omega)", 1.);
+  ctl->AddGenerator(iaom, "Injector (AntiOmega)", 1.);
+  return ctl;
+}
