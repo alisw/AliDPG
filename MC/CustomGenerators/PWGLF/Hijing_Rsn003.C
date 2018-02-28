0def40b9e73bbea9bde83e2dc4023452daf83840
Maximiliano Puccio <maximiliano.puccio@cern.ch>
2018-02-28 02:41:17 +0100

    From AliGenBox to AliGenParam

--- MC/CustomGenerators/PWGLF/Hijing_Rsn003.C
+++ MC/CustomGenerators/PWGLF/Hijing_Rsn003.C
@@ -1,3 +1,12 @@
+AliGenParam* GeneratorParam(int n, int pdg, double ptmin, double ptmax, double ymin, double ymax, AliDecayer* dec = 0x0) {
+  AliGenParam* gen = new AliGenParam(Form("%i",pdg), n, pdg);
+  gen->SetYRange(ymin,ymax);
+  gen->SetPtRange(ptmin,ptmax);
+  if (dec) gen->SetDecayer(dec);
+  return gen;
+}
+
+
 AliGenerator *GeneratorCustom(TString opt = "")
 {
   //
@@ -9,24 +18,26 @@ AliGenerator *GeneratorCustom(TString opt = "")
   TString optList[3] = {"a", "b", "c"};
   Int_t ninjlist[3]  = {10, 6, 3};
   Int_t ninj = 1;
-  
+
   Double_t pTmax = 25.0;
   if(opt.EqualTo(optList[0]) || opt.EqualTo(optList[1])) pTmax = 25.0;
   else if (opt.EqualTo(optList[2])) pTmax = 20.0;
-  
+
   for (Int_t iopt = 0; iopt < 3; iopt++)
     if (opt.EqualTo(optList[iopt]))
       ninj = ninjlist[iopt];
-  
+
+  AliDecayerPythia *dec = new AliDecayerPythia;
+
   AliGenCocktail *ctl  = GeneratorCocktail("Hijing_Rsn003");
   AliGenerator   *hij  = GeneratorHijing();
-  AliGenerator   *inj1 = GeneratorInjector(ninj,  333, 0., pTmax, -0.6, 0.6);
-  AliGenerator   *inj2 = GeneratorInjector(ninj,  313, 0., pTmax, -0.6, 0.6);
-  AliGenerator   *inj3 = GeneratorInjector(ninj, -313, 0., pTmax, -0.6, 0.6);
-  
+  AliGenerator   *inj1 = GeneratorParam(ninj,  333, 0., pTmax, -0.6, 0.6, dec);
+  AliGenerator   *inj2 = GeneratorParam(ninj,  313, 0., pTmax, -0.6, 0.6, dec);
+  AliGenerator   *inj3 = GeneratorParam(ninj, -313, 0., pTmax, -0.6, 0.6, dec);
+
   ctl->AddGenerator(hij,  "Hijing",            1.);
-  ctl->AddGenerator(inj1, "Injector (RsnPhi)", 1.);
   ctl->AddGenerator(inj2, "Injector (RsnKSt0)", 1.);
+  ctl->AddGenerator(inj1, "Injector (RsnPhi)", 1.);
   ctl->AddGenerator(inj3, "Injector (RsnAKst0)", 1.);
   return ctl;
 }
