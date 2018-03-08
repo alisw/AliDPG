473dffd2b063512fa85807c0053296281c33b676
Maximiliano Puccio <maximiliano.puccio@cern.ch>
2018-03-08 14:50:29 +0100

    Move Resonance p-Pb generator to AliGenParam

--- MC/CustomGenerators/PWGLF/Dpmjet_Rsn_pPb001.C
+++ MC/CustomGenerators/PWGLF/Dpmjet_Rsn_pPb001.C
@@ -1,14 +1,21 @@
+AliGenParam* GeneratorParam(int n, int pdg, double ptmin, double ptmax, double ymin, double ymax, AliDecayer* dec = 0x0) {
+  AliGenParam* gen = new AliGenParam(Form("%i",pdg), n, pdg);
+  gen->SetYRange(ymin,ymax);
+  gen->SetPtRange(ptmin,ptmax);
+  if (dec) gen->SetDecayer(dec);
+  return gen;
+}
 
 AliGenerator *GeneratorCustom(TString opt = "")
 {
-  // Custom generator to inject phi,K*0,A-K*0, K*+-, Lambda*, A-Lambda*, f0, f2 resonances 
+  // Custom generator to inject phi,K*0,A-K*0, K*+-, Lambda*, A-Lambda*, f0, f2 resonances
   // Options:
   // pPb:  injection landau in pT up to  21 GeV/c; 3 different species per event;
   // rapidity range: -0.6 < y < 0.1 : Anchored to period LHC16r
 
   // Pbp: injection same as pPb, but rapidity range -0.1 < y < 0.6 : Anchored to period LHC16s
 
-    
+
   Int_t ninj = 1; // 1 particle per species
 
   // Set pT range (pT_L, pT_H)
@@ -23,7 +30,7 @@ AliGenerator *GeneratorCustom(TString opt = "")
 
   Double_t  pT_H = pT_L + 0.0005;
 
- 
+
   // Set Rapidity
   Double_t y_L = 0, y_H = 0;
   TString Select[2] = {"pPb","Pbp"};
@@ -37,12 +44,14 @@ AliGenerator *GeneratorCustom(TString opt = "")
   AliGenDPMjet *dpmjet  = (AliGenDPMjet*) GeneratorPhojet();
   ctl->AddGenerator(dpmjet,"Dpmjet",           1.);
 
+  AliDecayerPythia *dec = new AliDecayerPythia;
+
   Double_t randm = gRandom->Rndm();
 
   if ( randm < 0.333) {
-    AliGenerator *injphi_pPb     = GeneratorInjector(ninj, 333, pT_L, pT_H,  y_L, y_H); // Phi 
-    AliGenerator *injkstar0_pPb  = GeneratorInjector(ninj, 313, pT_L, pT_H,  y_L, y_H); // K*0
-    AliGenerator *injkstarp_pPb  = GeneratorInjector(ninj, 323, pT_L, pT_H,  y_L, y_H); // K*+
+    AliGenerator *injphi_pPb     = GeneratorParam(ninj, 333, pT_L, pT_H,  y_L, y_H, dec); // Phi
+    AliGenerator *injkstar0_pPb  = GeneratorParam(ninj, 313, pT_L, pT_H,  y_L, y_H, dec); // K*0
+    AliGenerator *injkstarp_pPb  = GeneratorParam(ninj, 323, pT_L, pT_H,  y_L, y_H, dec); // K*+
 
     ctl->AddGenerator(injphi_pPb,"Injector(RsnPhi)", 1.);
     ctl->AddGenerator(injkstar0_pPb,"Injector(Rsnkstar0)", 1.);
@@ -50,9 +59,9 @@ AliGenerator *GeneratorCustom(TString opt = "")
  }
 
   else if ( randm >= 0.333 && randm < 0.667) {
-    AliGenerator *injlstar_pPb   = GeneratorInjector(ninj, 3124, pT_L, pT_H,  y_L, y_H); // Lambda* 
-    AliGenerator *injakstar0_pPb = GeneratorInjector(ninj, -313, pT_L, pT_H,  y_L, y_H); // A-K*0
-    AliGenerator *injkstarm_pPb  = GeneratorInjector(ninj, -323, pT_L, pT_H,  y_L, y_H); // K*-
+    AliGenerator *injlstar_pPb   = GeneratorParam(ninj, 3124, pT_L, pT_H,  y_L, y_H, dec); // Lambda*
+    AliGenerator *injakstar0_pPb = GeneratorParam(ninj, -313, pT_L, pT_H,  y_L, y_H, dec); // A-K*0
+    AliGenerator *injkstarm_pPb  = GeneratorParam(ninj, -323, pT_L, pT_H,  y_L, y_H, dec); // K*-
 
     ctl->AddGenerator(injlstar_pPb,"Injector(Rsnlstar)", 1.);
     ctl->AddGenerator(injakstar0_pPb,"Injector(Rsnakstar0)", 1.);
@@ -60,9 +69,9 @@ AliGenerator *GeneratorCustom(TString opt = "")
   }
 
   else {
-    AliGenerator *injalstar_pPb = GeneratorInjector(ninj,  -3124, pT_L, pT_H,  y_L, y_H); // A-Lambda* 
-    AliGenerator *injf0_pPb     = GeneratorInjector(ninj,9010221, pT_L, pT_H,  y_L, y_H); // f0(980)
-    AliGenerator *injf2_pPb     = GeneratorInjector(ninj,    225, pT_L, pT_H,  y_L, y_H); // f2(1270)
+    AliGenerator *injalstar_pPb = GeneratorParam(ninj,  -3124, pT_L, pT_H,  y_L, y_H, dec); // A-Lambda*
+    AliGenerator *injf0_pPb     = GeneratorParam(ninj,9010221, pT_L, pT_H,  y_L, y_H, dec); // f0(980)
+    AliGenerator *injf2_pPb     = GeneratorParam(ninj,    225, pT_L, pT_H,  y_L, y_H, dec); // f2(1270)
 
     ctl->AddGenerator(injalstar_pPb,"Injector(Rsnalstar)", 1.);
     ctl->AddGenerator(injf0_pPb,"Injector(Rsnf0)", 1.);
