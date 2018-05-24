9367e068528fc6a1bd34b773e1543445e663c21e
rbertens <rbertens@cern.ch>
2018-05-24 10:29:38 +0200

    switch from nightlies to official builds

--- MC/EXTRA/gen_therm2.sh
+++ MC/EXTRA/gen_therm2.sh
@@ -102,7 +102,7 @@ fi
 echo "running in `pwd`, writing HepMC to $1"
 
 # prepare environment
-eval $(/cvmfs/alice-nightlies.cern.ch/bin/alienv printenv Therminator2::v2.0.3-alice1_PWGMMTEST-2)
+eval $(/cvmfs/alice.cern.ch/bin/alienv printenv Therminator2::v2.0.3-alice1-1)
 
 if [ -d events/ ];
 then
