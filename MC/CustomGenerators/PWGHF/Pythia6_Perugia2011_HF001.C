b78ac6221bd7093f323afd35b0478660d229eb6e
Michael Weber <m.weber@cern.ch>
2018-08-22 17:27:13 +0200

    Adding also the upper threshold for injected partcles as variable

--- MC/CustomGenerators/PWGHF/Pythia6_Perugia2011_HF001.C
+++ MC/CustomGenerators/PWGHF/Pythia6_Perugia2011_HF001.C
@@ -20,10 +20,18 @@ GeneratorCustom(TString opt = "")
   //
   AliGenerator *phf  = GeneratorPythia6Heavy(process[iprocess], decay[idecay], kPythia6Tune_Perugia2011, kFALSE);
   ctl->AddGenerator(phf, label[iprocess][idecay], 1.);
+  //
+  Double_t ptMaxInjected = 50.;
+  if(ptmaxConfig != -1){
+    ptMaxInjected = ptmaxConfig;
+  }
 
   if(idecay == 3){ //apply cut on pte (steered by pttrigminConfig)
     ((AliGenPythia *)phf)->SetChildPtRange(pttrigminConfig, 10000.);
-    printf(">>>>> added HF generator %s with pT cut = %.2f \n", label[iprocess][idecay], pttrigminConfig);
+    printf(">>>>> added HF generator %s with pT cut = %.2f and ptMaxInjected = %.2f\n", label[iprocess][idecay], pttrigminConfig,ptMaxInjected);
+  }
+  else if(decay[idecay] == kPythia6HeavyDecay_Electron){
+    printf(">>>>> added HF generator %s with ptMaxInjected = %.2f\n \n", label[iprocess][idecay],ptMaxInjected);
   }
   else{
     printf(">>>>> added HF generator %s \n", label[iprocess][idecay]);
@@ -36,36 +44,26 @@ GeneratorCustom(TString opt = "")
     AliGenParam *pi0 = new AliGenParam(2, plib, AliGenPHOSlib::kPi0Flat);
     pi0->SetPhiRange(0., 360.) ;
     pi0->SetYRange(-1.2, 1.2) ;
+    pi0->SetPtRange(0., ptMaxInjected);
     if(idecay == 3){
-      pi0->SetPtRange(0., 80.) ;
       //select electrons from decay
       pi0->SetCutOnChild(1);
       pi0->SetPdgCodeParticleforAcceptanceCut(11);
       pi0->SetChildYRange(-1.2, 1.2);
       pi0->SetChildPtRange(pttrigminConfig, 10000.);
-      Printf("pi0: Apply pt cut %f",pttrigminConfig);
-    }
-    else{
-      pi0->SetPtRange(0., 50.) ;
-      Printf("pi0: Don't apply pt cut %f",pttrigminConfig);
     }
     ctl->AddGenerator(pi0,  "pi0", 1.);
 
     AliGenParam *eta = new AliGenParam(1, plib, AliGenPHOSlib::kEtaFlat);
     eta->SetPhiRange(0., 360.) ;
     eta->SetYRange(-1.2, 1.2) ;
+    eta->SetPtRange(0., ptMaxInjected);
     if(idecay == 3){
-      eta->SetPtRange(0., 80.) ;
       //select electrons from decay
       eta->SetCutOnChild(1);
       eta->SetPdgCodeParticleforAcceptanceCut(11);
       eta->SetChildYRange(-1.2, 1.2);
       eta->SetChildPtRange(pttrigminConfig, 10000.);
-      Printf("eta: Apply pt cut %f",pttrigminConfig);
-    }
-    else{
-      eta->SetPtRange(0., 50.) ;
-      Printf("eta: Don't apply pt cut %f",pttrigminConfig);
     }
     ctl->AddGenerator(eta,  "eta", 1.);
   }
