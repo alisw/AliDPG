ffcc8e16fdcf07cbdd6d9dc748323e9859bfccd3
Dario Berzano <dario.berzano@cern.ch>
2018-09-11 08:23:06 +0200

    Set different vmem ulimit on ROOT 5/6

--- DataProc/CPass0/runCPass0.sh
+++ DataProc/CPass0/runCPass0.sh
@@ -71,8 +71,12 @@ runNum=`echo "$runNumF" |  sed 's/^0*//'`
 # Exporting variable to define that we are in CPass0 to be used in reconstruction
 export CPass='0'
 
-# Set memory limits to a value lower than the hard site limits to at least get the logs of failing jobs
-ulimit -S -v 3500000
+# Set max vmem [kb] even if Grid jobs have it, so that logs are saved on failure
+# ROOT 6+: 4 GB, ROOT 5: 3.5 GB (rootcling n/a on ROOT 5)
+[[ -x $ROOTSYS/bin/rootcling ]] && ALI_ULIMIT_VMEM=4000000 || ALI_ULIMIT_VMEM=3500000
+echo "Capping vmem to $ALI_ULIMIT_VMEM kb: processing will be killed after that threshold!"
+ulimit -S -v $ALI_ULIMIT_VMEM
+unset $ALI_ULIMIT_VMEM
 
 # run TPC clusterization in separate process before reconstruction
 export preclusterizeTPC='0'
