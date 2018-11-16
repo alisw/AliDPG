5cc0af8b02fa3e00ebf3c4ac2972fd1ae001fa82
zampolli <Chiara.Zampolli@cern.ch>
2018-11-16 10:34:39 +0100

    removing ulimit settings

--- DataProc/CPass0/runCPass0.sh
+++ DataProc/CPass0/runCPass0.sh
@@ -71,13 +71,6 @@ runNum=`echo "$runNumF" |  sed 's/^0*//'`
 # Exporting variable to define that we are in CPass0 to be used in reconstruction
 export CPass='0'
 
-# Set max vmem [kb] for ROOT 5 processing (no ulimit for ROOT 6)
-if [[ -x $ROOTSYS/bin/rootcling ]]; then
-  echo "ROOT 6+ detected: not capping vmem"
-else
-  echo "Capping vmem to 3.5 GB"
-  ulimit -S -v 3500000
-fi
 ulimit -a
 
 # run TPC clusterization in separate process before reconstruction
