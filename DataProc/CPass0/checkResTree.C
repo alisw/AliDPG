bb20d7af65cd884c5f21ce309b0572daf2666b76
shahoian <ruben.shahoyan@cern.ch>
2018-11-18 15:59:56 +0100

    macro to check the integrity of residual tree
    In case of corruption will write to stdout CORRUPTED Residuals file, ev ...

---
+++ DataProc/CPass0/checkResTree.C
@@ -0,0 +1,18 @@
+#if !(defined(__CLING__)  || defined(__CINT__)) || defined(__ROOTCLING__) || defined(__ROOTCINT__)
+#include <TTree.h>
+#include <TFile.h>
+#endif
+
+void checkResTree(const char* fname = "ResidualTrees.root", const char* tname = "delta")
+{
+  TFile tf(fname);
+  TTree* tr = (TTree*)tf.Get(tname); // we want macro to crash if the tree is not there
+  int nev = tr->GetEntries();
+  int rdb = 0;
+  for (int i=0;i<nev;i++) {
+    rdb = tr->GetEntry(i);
+    if (rdb<=0 || rdb>10000) {
+      printf("CORRUPTED Residuals file, ev %d got %d bytes\n",i,rdb);
+    } 
+  }
+}
