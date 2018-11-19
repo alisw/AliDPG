8ad5b5ae57f7c2c1a948ec3e678427194064f083
zampolli <Chiara.Zampolli@cern.ch>
2018-11-19 10:25:47 +0100

    Incorporating the check for the integrity of the ResTree in the script

--- DataProc/CPass0/checkResTree.C
+++ DataProc/CPass0/checkResTree.C
@@ -9,10 +9,18 @@ void checkResTree(const char* fname = "ResidualTrees.root", const char* tname = 
   TTree* tr = (TTree*)tf.Get(tname); // we want macro to crash if the tree is not there
   int nev = tr->GetEntries();
   int rdb = 0;
+  bool corrupted = kFALSE;
   for (int i=0;i<nev;i++) {
     rdb = tr->GetEntry(i);
     if (rdb<=0 || rdb>10000) {
       printf("CORRUPTED Residuals file, ev %d got %d bytes\n",i,rdb);
+      corrupted = kTRUE;
     } 
+  }
+  if (corrupted) {
+    printf("The residual tree file is corrupted\n");
+  }
+  else {
+    printf("The residual tree file is OK\n");
   }
 }
