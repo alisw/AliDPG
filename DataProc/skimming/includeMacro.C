a078b27fb07369f912d0036eff3d1c98d585f612
shahoian <ruben.shahoyan@cern.ch>
2018-12-19 00:45:08 +0100

    added code for CALOFAST skimming

---
+++ DataProc/skimming/includeMacro.C
@@ -0,0 +1,10 @@
+#include "RVersion.h"
+
+void includeMacro() {
+  #if ROOT_VERSION_CODE < ROOT_VERSION(6,0,0)
+  // ROOT 5-only workarounds: set the appropriate include paths and trigger
+  // preloading of the required libraries before invoking ACLiC on the
+  // processing macro
+  gSystem->SetIncludePath("-I$ALICE_PHYSICS/include -I$ALICE_ROOT/include");
+  #endif
+}
