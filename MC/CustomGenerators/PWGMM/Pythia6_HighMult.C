aad464ca2b5dd1f94d5411decb505815edef4dec
Michael Weber <m.weber@cern.ch>
2019-02-28 22:08:59 +0100

    Cast to AliGenPythia

--- MC/CustomGenerators/PWGMM/Pythia6_HighMult.C
+++ MC/CustomGenerators/PWGMM/Pythia6_HighMult.C
@@ -3,9 +3,10 @@ GeneratorCustom()
 { 
       comment = comment.Append(" pp: Pythia 6 (Perugia2011 tune) high mult");
 
-      AliGenerator *pythia = GeneratorPythia6(kPythia6Tune_Perugia2011); 
-      
-      pythia->SetTriggerChargedMultiplicity(60, 1.4) //  multiplicity threshold set to 60 in |eta| < 1.4
+      AliGenPythia* pythia = (AliGenPythia*)GeneratorPythia6(kPythia6Tune_Perugia2011); 
+
+      // multiplicity threshold set to 60 in |eta| < 1.4
+      pythia->SetTriggerChargedMultiplicity(60, 1.4);
       
       return pythia;
 }
