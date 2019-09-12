0b932fd83e3bd391bfb72c7e4d747e4f1fd8ba3c
ytwu <torrence@mail.ustc.edu.cn>
2019-09-12 21:13:20 +0800

    [ALIROOT-8339] PWGDQ/Jpsi2ee - FIX pythia generator for jets production

--- MC/CustomGenerators/PWGDQ/Pythia6_Perugia2011_Jpsi2ee13TeV.C
+++ MC/CustomGenerators/PWGDQ/Pythia6_Perugia2011_Jpsi2ee13TeV.C
@@ -2,7 +2,7 @@ AliGenerator *
 GeneratorCustom()
 {
   AliGenCocktail *ctl   = GeneratorCocktail("Pythia6_Perugia2011_Jpsiee001");
-  AliGenerator   *pyt   = GeneratorPythia6(kPythia6Tune_Perugia2011);
+  AliGenerator   *pyt   = GeneratorPythia6Jets(kPythia6Tune_Perugia2011);
   ctl->AddGenerator(pyt,  "Pythia6", 1.);
   if (uidConfig % 10 < 7) {
     AliGenerator *jpsi  = Generator_Jpsiee("pp 5.03", 0.7, 0.0, 0.3, 0.0, 0.0);
