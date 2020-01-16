cddcca655e3150bb60a282a35964e6d3d6ee25f7
Fabio Colamaria <fabiocolamaria@cern.ch>
2020-01-16 14:26:23 +0100

    Updated b-jet generators adding cases with EvtGen for b-hadoon decays

--- MC/CustomGenerators/PWGHF/PYTHIA_HFjetpp.C
+++ MC/CustomGenerators/PWGHF/PYTHIA_HFjetpp.C
@@ -1,14 +1,17 @@
 AliGenerator *
 GeneratorCustom(TString opt = "")
 {
+    // set the xmldoc path using PYTHIA8DATA enviroement var
+    gSystem->Setenv("PYTHIA8DATA", gSystem->ExpandPathName("$ALICE_ROOT/PYTHIA8/pythia8243/xmldoc"));
+
     AliGenCocktail *ctl  = GeneratorCocktail("PYTHIA_HF");
     
     Int_t process[2] = {kPythia6HeavyProcess_Charm, kPythia6HeavyProcess_Beauty};
-    Int_t decay[4]   = {kPythia6HeavyDecay_Hadrons, kPythia6HeavyDecay_HadronsWithV0, kPythia6HeavyDecay_Electron, kPythia6HeavyDecay_All};
+    Int_t decay[5]   = {kPythia6HeavyDecay_Hadrons, kPythia6HeavyDecay_HadronsWithV0, kPythia6HeavyDecay_Electron, kPythia6HeavyDecay_All, kPythia6HeavyDecay_All_bDecaysEvtGen};
     
-    const Char_t *label[2][4] = {
-        "chadr PYTHIA", "chadr PYTHIA", "cele PYTHIA", "ccbar PYTHIA",
-        "bchadr PYTHIA", "bchadr PYTHIA", "bele PYTHIA", "bbbar PYTHIA"
+    const Char_t *label[2][5] = {
+        "chadr PYTHIA", "chadr PYTHIA", "cele PYTHIA", "ccbar PYTHIA", "ccbar PYTHIA+EvtGen",
+        "bchadr PYTHIA", "bchadr PYTHIA", "bele PYTHIA", "bbbar PYTHIA", "bbbar PYTHIA+EvtGen"
     };
     
     Int_t iprocess = -1;
@@ -17,23 +20,27 @@ GeneratorCustom(TString opt = "")
     else if(opt.Contains("bb")) {iprocess = 1; printf("-- HF process = %s -- \n",opt.Data());}
     else {printf("-- no HF process set = %s -- \n",opt.Data());}
     
-    TString optList[5] = {"had", "hv0", "ele", "all", "jetjet"};
+    TString optList[6] = {"had", "hv0", "ele", "all", "bEvtGen", "jetjet"};
     Int_t idecay = 0;
-    for (Int_t iopt = 0; iopt < 5; iopt++){
+    for (Int_t iopt = 0; iopt < 6; iopt++){
         if (opt.Contains(optList[iopt]))
             idecay = iopt;
     }
     
     AliGenerator *phf;
-    if(idecay < 4) {
+    if(idecay < 5) {
         phf  = GeneratorPythia6Heavy(process[iprocess], decay[idecay], kPythia6Tune_Perugia2011, kFALSE);
         ctl->AddGenerator(phf, label[iprocess][idecay], 1.);
         printf(">>>>> added generator for HF production:  %s \n", label[iprocess][idecay]);
     }
-    else if(idecay == 4) {
+    else if(idecay == 5) {
         phf  = GeneratorPythia6Jets(kPythia6Tune_Perugia2011);
         ctl->AddGenerator(phf, "jetjet", 1.);
         printf(">>>>> added generator for HF production:  %s \n", "jetjet");
     }
+
+    AliGenEvtGen *gener = new AliGenEvtGen();
+    gener->SetParticleSwitchedOff(AliGenEvtGen::kBeautyPart);
+    ctl->AddGenerator(gener,"EvtGen",1.);
     return ctl;
 }
