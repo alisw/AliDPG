540dc3bfe633455f0dfb6d0ca536002f3bb6827d
Francesco Prino <prino@to.infn.it>
2020-01-24 11:55:01 +0100

    Additional options to set the vertex from command arguments

--- MC/GeneratorConfig.C
+++ MC/GeneratorConfig.C
@@ -423,7 +423,7 @@ void GeneratorConfig(Int_t tag)
       Printf("Simulation with vertex position and sigma from Config file");
       Float_t vx,vy,vz;
       gen->GetOrigin(vx,vy,vz);
-      Printf(" <xv>=%.2f  <yv>=%.2f  <zv>=%.2f",vx,vy,vz);
+      Printf(" <xv>=%.3f  <yv>=%.3f  <zv>=%.3f cm",vx,vy,vz);
       customDiam=kTRUE;
     }
     else if(strcmp(gSystem->Getenv("CONFIG_GENVERT"), "NominalVertex") == 0){
@@ -432,6 +432,34 @@ void GeneratorConfig(Int_t tag)
       gen->SetSigma(0.,0.,0.);
       gen->SetVertexSmear(kNoSmear);
       customDiam=kTRUE;
+    }
+    else if(strstr(gSystem->Getenv("CONFIG_GENVERT"), "FixedIn") != 0){
+      Float_t vx,vy,vz;
+      Int_t ncoord=sscanf(gSystem->Getenv("CONFIG_GENVERT"),"FixedIn(%f,%f,%f)",&vx,&vy,&vz);
+      if(ncoord==3){
+	Printf("Simulation with vertex position fixed from command argument");
+	Printf(" xv=%.3f  yv=%.3f  zv=%.3f cm",vx,vy,vz);
+	gen->SetOrigin(vx,vy,vz);
+	gen->SetSigma(0.,0.,0.);
+	gen->SetVertexSmear(kNoSmear);
+	customDiam=kTRUE;
+      }else{
+	Printf("Failed to extract vertex position from command argument --genvert %s",gSystem->Getenv("CONFIG_GENVERT"));
+      }
+    }
+    else if(strstr(gSystem->Getenv("CONFIG_GENVERT"), "Gauss") != 0){
+      Float_t vx,vy,vz,sx,sy,sz;
+      Int_t ncoord=sscanf(gSystem->Getenv("CONFIG_GENVERT"),"GaussMean(%f,%f,%f)Sigma(%f,%f,%f)",&vx,&vy,&vz,&sx,&sy,&sz);
+      if(ncoord==6){
+	Printf("Simulation with gaussian vertex from command argument");
+	Printf(" <xv>=%.3f  <yv>=%.3f  <zv>=%.3f cm --- sigmax=%.3f  sigmay=%.3f  sigmaz=%.3f cm",vx,vy,vz,sx,sy,sz);
+	gen->SetOrigin(vx,vy,vz);
+	gen->SetSigma(sx,sy,sz);
+	gen->SetVertexSmear(kPerEvent);
+	customDiam=kTRUE;
+      }else{
+	Printf("Failed to extract vertex mean and sigma from command argument --genvert %s",gSystem->Getenv("CONFIG_GENVERT"));
+      }
     }
   }
   if(!customDiam){
