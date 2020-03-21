64b6f1bfb4dff74f45505b1caf43adda83087c1c
sashingo <shingo.sakai@cern.ch>
2020-03-21 19:04:47 +0900

    make new PWGHF config for W->e production (#351)

---
+++ MC/CustomGenerators/PWGHF/Pythia6_Perugia2011_Wboson.C
@@ -0,0 +1,22 @@
+AliGenerator *GeneratorCustom()
+
+{
+
+  Int_t triggerPart = 11;
+  if (uidConfig%2 == 0)
+      {
+       triggerPart = 11;
+      }
+  else
+      {
+       triggerPart = -11;
+      }
+
+  AliGenPythia* pyth = GeneratorPythia6(kPythia6Tune_Perugia2011);
+  pyth->SetProcess(kPyW);
+  pyth->SetTriggerParticle(triggerPart,-1.0,1.0,8,100);   // take event that electrons in EMCal
+  return pyth;
+
+}
+
+
