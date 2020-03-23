6bf81fc04fe3ff57b587a0321faaf6ddc1afb288
Maximiliano Puccio <mpuccio@users.noreply.github.com>
2020-03-23 07:56:07 +0100

    Add Hijing configuration for LamndaN and LambdaNN (#352)

---
+++ MC/CustomGenerators/PWGLF/Hijing_Exo.C
@@ -0,0 +1,17 @@
+AliGenerator * GeneratorCustom()
+{
+  AliGenCocktail *ctl   = GeneratorCocktail("LambdaN");
+
+  AliGenerator   *hij   = GeneratorHijing();
+  ctl->AddGenerator(hij,  "Hijing", 1.);
+
+  AliGenerator   *LN  = Generator_Nuclex(0x2020, kFALSE, 20);
+  ctl->AddGenerator(LN,"Nuclex_LambdaN", 1.);
+
+  AliGenerator   *aLN  = Generator_Nuclex(0x2020, kTRUE, 20);
+  ctl->AddGenerator(aLN,"Nuclex_AntiLambdaN", 1.);
+
+  return ctl;
+
+}
+
