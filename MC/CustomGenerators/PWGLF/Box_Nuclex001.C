b9d2bbc2d379997f68fc87aa6840d2b7a146c7c7
Maximiliano Puccio <mpuccio@users.noreply.github.com>
2020-05-06 22:39:45 +0200

    Update pt limits (#363)

--- MC/CustomGenerators/PWGLF/Box_Nuclex001.C
+++ MC/CustomGenerators/PWGLF/Box_Nuclex001.C
@@ -17,13 +17,14 @@ GeneratorCustom()
     "Triton",
     "Helium-4"
   };
+  double maxpt[5] = {5.,5.,10.,5.,10.};
   for (Int_t ipart = 0; ipart < 5; ++ipart) {
     for (Int_t negative = 0; negative < 2; negative++) {
       AliGenBox *box = new AliGenBox(1);
       Int_t pdg = pdgcodes[ipart];
       if (negative) pdg = -pdg;
       box->SetPart(pdg);
-      box->SetPtRange(0., 5.);
+      box->SetPtRange(0., maxpt[ipart]);
       box->SetPhiRange(0., 360.);
       box->SetYRange(-0.8,0.8);
       ctl->AddGenerator(box, Form("%s%i",names[ipart],negative), 1);
