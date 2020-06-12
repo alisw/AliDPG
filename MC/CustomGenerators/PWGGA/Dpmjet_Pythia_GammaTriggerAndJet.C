165315aa7b5040b35d698eb142a13924c1c42674
Gustavo Conesa <Gustavo.Conesa.Balbastre@cern.ch>
2020-06-12 10:04:40 +0200

    avoid unnecessary casting line

--- MC/CustomGenerators/PWGGA/Dpmjet_Pythia_GammaTriggerAndJet.C
+++ MC/CustomGenerators/PWGGA/Dpmjet_Pythia_GammaTriggerAndJet.C
@@ -27,8 +27,7 @@ GeneratorCustom
 (TString opt = "kFullDetector")
 {
   // Init cocktail
-  AliGenerator   *gen = GeneratorCocktail(Form("Dpmjet_%s",processConfig.Data()));
-  AliGenCocktail *ctl = (AliGenCocktail*) gen;
+  AliGenCocktail *ctl = (AliGenCocktail*) GeneratorCocktail(Form("Dpmjet_%s",processConfig.Data()));
   
   //
   // DPMjet
@@ -99,5 +98,5 @@ GeneratorCustom
   
   ctl->AddGenerator(gammajet, processConfig, 1.);  
   
-  return gen;
+  return ctl;
 }
