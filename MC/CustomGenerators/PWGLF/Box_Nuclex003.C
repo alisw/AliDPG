d46105a514349c132409baca11f575d6411c7b8a
akalweit <Alexander.Philipp.Kalweit@cern.ch>
2020-09-08 21:58:14 +0200

    Add files via upload
    Adding additional box generator needed for ALIROOT-8542

---
+++ MC/CustomGenerators/PWGLF/Box_Nuclex003.C
@@ -0,0 +1,16 @@
+AliGenerator *
+GeneratorCustom()
+{
+    AliGenCocktail *ctl = (AliGenCocktail*) GeneratorCocktail("Box_Nuclex003");
+    //
+    AliGenBox *box = new AliGenBox(160);
+    Int_t pdg = -1000020030;
+    box->SetPart(pdg);
+    box->SetPtRange(0., 12.);
+    box->SetPhiRange(0., 360.);
+    box->SetYRange(-0.8, 0.8);
+    //
+    ctl->AddGenerator(box, "anti-3He", 1.0);
+    //
+
+}
