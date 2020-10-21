d630921b11e85f847442bed38e9cb1d2f851ffbb
Gustavo Conesa <Gustavo.Conesa.Balbastre@cern.ch>
2020-10-21 15:20:02 +0200

    Pythia8 shoving configuration

---
+++ MC/CustomGenerators/PWGCF/Pythia8_Monash2013_Shoving.C
@@ -0,0 +1,31 @@
+AliGenerator *
+GeneratorCustom(TString opt = "")
+{
+  AliGenerator *py8 = GeneratorPythia8(kPythia8Tune_Monash2013);
+  
+  Float_t fgAmplitude = 4;
+  
+  (AliPythia8::Instance())->ReadString("SoftQCD:nonDiffractive = on");
+  (AliPythia8::Instance())->ReadString("SoftQCD:singleDiffractive = on");
+  (AliPythia8::Instance())->ReadString("SoftQCD:doubleDiffractive = on");
+  (AliPythia8::Instance())->ReadString("Ropewalk:RopeHadronization = on"); 
+  (AliPythia8::Instance())->ReadString("Ropewalk:doShoving = on"); 
+  (AliPythia8::Instance())->ReadString("Ropewalk:doFlavour = off"); 
+  (AliPythia8::Instance())->ReadString("Ropewalk:rCutOff = 10.0"); 
+  (AliPythia8::Instance())->ReadString("Ropewalk:limitMom = on"); 
+  (AliPythia8::Instance())->ReadString(Form("Ropewalk:pTcut = %lf",2.0)); 
+  (AliPythia8::Instance())->ReadString("Ropewalk:r0 = 0.41"); 
+  (AliPythia8::Instance())->ReadString("Ropewalk:m0 = 0.2"); 
+  (AliPythia8::Instance())->ReadString(Form("Ropewalk:gAmplitude = %lf",fgAmplitude)); 
+  (AliPythia8::Instance())->ReadString("Ropewalk:gExponent = 1.0"); 
+  (AliPythia8::Instance())->ReadString("Ropewalk:deltat = 0.1"); 
+  (AliPythia8::Instance())->ReadString("Ropewalk:tShove = 1."); 
+  (AliPythia8::Instance())->ReadString("Ropewalk:deltay = 0.1"); 
+  (AliPythia8::Instance())->ReadString("Ropewalk:tInit = 1.5"); 
+  // Enabling setting of vertex information.
+  (AliPythia8::Instance())->ReadString("PartonVertex:setVertex = on"); 
+  (AliPythia8::Instance())->ReadString("PartonVertex:protonRadius = 0.7"); 
+  (AliPythia8::Instance())->ReadString("PartonVertex:emissionWidth = 0.1"); 
+  
+  return py8;
+}
