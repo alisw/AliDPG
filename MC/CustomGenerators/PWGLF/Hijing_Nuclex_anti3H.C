c7669698562db6fb390e23023cdd78bfd0709233
Maximiliano Puccio <mpuccio@users.noreply.github.com>
2020-11-04 07:42:59 +0100

    Change pt limit

--- MC/CustomGenerators/PWGLF/Hijing_Nuclex_anti3H.C
+++ MC/CustomGenerators/PWGLF/Hijing_Nuclex_anti3H.C
@@ -5,7 +5,7 @@ GeneratorCustom()
   AliGenCocktail *ctl   = (AliGenCocktail*) GeneratorCocktail("Hijing_Nuclex_anti3H");
   AliGenerator   *hij   = GeneratorHijing();
   ctl->AddGenerator(hij,  "Hijing", 1.);
-  AliGenerator   *nu1a  = Generator_Nuclex(0x4, kTRUE, 160, 12., 0.8);
+  AliGenerator   *nu1a  = Generator_Nuclex(0x4, kTRUE, 160, 4., 0.8);
   ctl->AddGenerator(nu1a,  "Anti-3H", 1.);
   return ctl;
 }
