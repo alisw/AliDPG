50a23fee38372f69f7a16c1b503075e71b72fbaf
catalinristea <catalin.ristea@live.com>
2021-02-09 23:28:24 +0200

    AOD macro: removed filter trees merging and corrected AOD PS EventStat_temp_AODFiltering (#455)
    Co-authored-by: Catalin Ristea <catalin.ristea@Catalins-MacBook-Pro.local>

--- AOD/main_AODtrainRawAndMC.C
+++ AOD/main_AODtrainRawAndMC.C
@@ -902,7 +902,7 @@ void AODmerge()
   TStopwatch timer;
   timer.Start();
   TString outputDir = "wn.xml";
-  TString outputFiles = "EventStat_temp.root,AODQA.root,AliAOD.root,AliAOD.VertexingHF.root,AliAOD.Muons.root,AliAODGammaConversion.root,AliAOD.Jets.root,FilterEvents_Trees.root,pyxsec_hists.root";
+  TString outputFiles = "EventStat_temp_AODFiltering.root,AODQA.root,AliAOD.root,AliAOD.VertexingHF.root,AliAOD.Muons.root,AliAODGammaConversion.root,AliAOD.Jets.root,pyxsec_hists.root";
   TString mergeExcludes = "";
   TObjArray *list = outputFiles.Tokenize(",");
   TIter *iter = new TIter(list);
