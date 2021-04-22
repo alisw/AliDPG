ee5c6334c0a73a1ea5050c1ba8bd8cb4876adf6a
bianchil <Livio.Bianchi@cern.ch>
2021-04-22 15:25:00 +0200

    Updated Generator with higher Xi pT reach (suitable for Lambda FD) and tuned stat

--- MC/CustomGenerators/PWGLF/Hijing_Str001.C
+++ MC/CustomGenerators/PWGLF/Hijing_Str001.C
@@ -4,7 +4,7 @@ GeneratorCustom(TString opt)
   TString optList[3] = {"a", "b", "c"};
   Int_t ninjk0[3] = {10, 5, 1};
   Int_t ninjla[3] = {10, 5, 1};
-  Int_t ninjxi[3] = {20, 8, 1};
+  Int_t ninjxi[3] = {40,16, 2};
   Int_t ninjom[3] = {17, 4, 1};
   Int_t iinj = 0;
   for (Int_t iopt = 0; iopt < 3; iopt++)
@@ -15,7 +15,7 @@ GeneratorCustom(TString opt)
   AliGenerator   *hij  = GeneratorHijing();
   AliGenerator   *ik0 = GeneratorInjector(ninjk0[iinj],         310, 0., 20., -0.7, 0.7);
   AliGenerator   *ila = GeneratorInjector(ninjla[iinj], sign * 3122, 0., 20., -0.7, 0.7);
-  AliGenerator   *ixi = GeneratorInjector(ninjxi[iinj], sign * 3312, 0., 12., -0.7, 0.7);
+  AliGenerator   *ixi = GeneratorInjector(ninjxi[iinj], sign * 3312, 0., 24., -0.7, 0.7);
   AliGenerator   *iom = GeneratorInjector(ninjom[iinj], sign * 3334, 0., 10., -0.7, 0.7);
   ctl->AddGenerator(hij,  "Hijing",            1.);
   ctl->AddGenerator(ik0, "Injector (K0s)", 1.);
