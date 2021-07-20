fc9b60bebc059fd5c55f05a5dbbf655ee5e8a35c
Gustavo Conesa Balbastre <Gustavo.Conesa.Balbastre@cern.ch>
2021-07-20 17:21:58 +0200

    add electron trigger pT case, by Shingo

--- MC/CustomGenerators/PWGHF/Hijing_HF002.C
+++ MC/CustomGenerators/PWGHF/Hijing_HF002.C
@@ -11,14 +11,14 @@ GeneratorCustom(TString opt = "")
   Int_t process[2] = {kPythia6HeavyProcess_Charm, kPythia6HeavyProcess_Beauty};
   Int_t decay[3]   = {kPythia6HeavyDecay_Hadrons, kPythia6HeavyDecay_HadronsWithV0, kPythia6HeavyDecay_Electron};
 
-  const Char_t *label[2][3] = {
-    "chadr PYTHIA", "chadr PYTHIA", "cele PYTHIA",
-    "bchadr PYTHIA", "bchadr PYTHIA", "bele PYTHIA"
+  const Char_t *label[2][4] = {
+    "chadr PYTHIA", "chadr PYTHIA", "cele PYTHIA", "cele PYTHIA eleTrig",
+    "bchadr PYTHIA", "bchadr PYTHIA", "bele PYTHIA", "bele PYTHIA eleTrig"
   };
   Int_t iprocess = uidConfig % 2;
-  TString optList[3] = {"had", "hv0", "ele"};
+  TString optList[4] = {"had", "hv0", "ele", "eleTrig"};
   Int_t idecay = 0;
-  for (Int_t iopt = 0; iopt < 3; iopt++){
+  for (Int_t iopt = 0; iopt < 4; iopt++){
     if (opt.EqualTo(optList[iopt]))
       idecay = iopt;
   }
@@ -27,6 +27,12 @@ GeneratorCustom(TString opt = "")
     //
     ctl->AddGenerator(phf, label[iprocess][idecay], 1.);
     printf(">>>>> added HF generator %s \n", label[iprocess][idecay]);
+
+    if(idecay == 3){ //apply cut on pte (steered by pttrigminConfig)
+    ( (AliGenPythia *)phf)->SetChildPtRange(pttrigminConfig, 10000.);
+       printf(">>>>> added HF generator %s with pT cut = %.2f\n", label[iprocess][idecay], pttrigminConfig);
+    }
+
     // add pi0 and eta enhancement
     if (decay[idecay] == kPythia6HeavyDecay_Electron) {
       AliGenPHOSlib *plib = new AliGenPHOSlib();
