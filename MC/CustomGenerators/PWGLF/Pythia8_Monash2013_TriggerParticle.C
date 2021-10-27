c59cfd31ea5c32cd238231959cee4496f21e5fbd
Alberto Caliva <alberto.caliva@cern.ch>
2021-10-27 22:04:33 +0200

    pythia8 monash 2013 + leading particle

--- MC/CustomGenerators/PWGLF/Pythia8_Monash2013_TriggerParticle.C
+++ MC/CustomGenerators/PWGLF/Pythia8_Monash2013_TriggerParticle.C
@@ -4,11 +4,6 @@ AliGenerator *GeneratorCustom (TString opt = "")  {
   
     //Leading Particle p_{T}
     Float_t pt_leading = 5.0;
-    
-    pythia->SetTriggerParticle(+2212,0.8,0.15,200.0);
-    pythia->SetTriggerParticle(-2212,0.8,0.15,200.0);
-    pythia->SetTriggerParticle(+2112,0.8,0.15,200.0);
-    pythia->SetTriggerParticle(-2112,0.8,0.15,200.0);
     pythia->SetTriggerParticleMinPt(pt_leading);
   
     return pythia;
