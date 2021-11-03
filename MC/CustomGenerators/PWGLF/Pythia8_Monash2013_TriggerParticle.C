78c350e644c186f7260f1da24e49066a00252291
Alberto Caliva <alberto.caliva@cern.ch>
2021-11-03 22:06:59 +0100

    added casting

--- MC/CustomGenerators/PWGLF/Pythia8_Monash2013_TriggerParticle.C
+++ MC/CustomGenerators/PWGLF/Pythia8_Monash2013_TriggerParticle.C
@@ -1,10 +1,11 @@
-AliGenerator *GeneratorCustom (TString opt = "")  {
+AliGenerator *GeneratorCustom ()  {
   
-    AliGenerator *pythia = GeneratorPythia8(kPythia8Tune_Monash2013);
+    AliGenPythiaPlus *pythia = (AliGenPythiaPlus*)GeneratorPythia8(kPythia8Tune_Monash2013);
   
     //Leading Particle p_{T}
     Float_t pt_leading = 5.0;
     pythia->SetTriggerParticleMinPt(pt_leading);
-  
+    pythia->SetHadronInBarrel(kTRUE);
+
     return pythia;
 }
