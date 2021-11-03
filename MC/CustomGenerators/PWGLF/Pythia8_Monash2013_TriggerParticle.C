54a29d4a4ecb5d2feab355726ffc215583ce3d10
Alberto Caliva <alberto.caliva@cern.ch>
2021-11-03 17:55:21 +0100

    Added casting to AliGenPythiaPlus

--- MC/CustomGenerators/PWGLF/Pythia8_Monash2013_TriggerParticle.C
+++ MC/CustomGenerators/PWGLF/Pythia8_Monash2013_TriggerParticle.C
@@ -1,12 +1,11 @@
 AliGenerator *GeneratorCustom ()  {
   
-    AliGenCocktail *ctl = (AliGenCocktail*) GeneratorCocktail("Pythia8_Monash2013_TriggerParticle");
     AliGenPythiaPlus *pythia = (AliGenPythiaPlus*)GeneratorPythia8(kPythia8Tune_Monash2013);
-
+  
     //Leading Particle p_{T}
     Float_t pt_leading = 5.0;
     pythia->SetTriggerParticleMinPt(pt_leading);
-    ctl->AddGenerator(pythia, "Pythia8 (Monash2013) Trigger Particle", 1);
+    pythia->SetHadronInBarrel(kTRUE);
 
-    return ctl;
+    return pythia;
 }
