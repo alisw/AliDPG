aa7a301eba8ae319990983869f64eec9d449774a
Alberto Caliva <alberto.caliva@cern.ch>
2021-11-03 13:15:08 +0100

    bug fix

--- MC/CustomGenerators/PWGLF/Pythia8_Monash2013_TriggerParticle.C
+++ MC/CustomGenerators/PWGLF/Pythia8_Monash2013_TriggerParticle.C
@@ -1,10 +1,12 @@
-AliGenerator *GeneratorCustom (TString opt = "")  {
+AliGenerator *GeneratorCustom ()  {
   
-    AliGenerator *pythia = GeneratorPythia8(kPythia8Tune_Monash2013);
-  
+    AliGenCocktail *ctl = (AliGenCocktail*) GeneratorCocktail("Pythia8_Monash2013_TriggerParticle");
+    AliGenPythiaPlus *pythia = (AliGenPythiaPlus*)GeneratorPythia8(kPythia8Tune_Monash2013);
+
     //Leading Particle p_{T}
     Float_t pt_leading = 5.0;
     pythia->SetTriggerParticleMinPt(pt_leading);
-  
-    return pythia;
+    ctl->AddGenerator(pythia, "Pythia8 (Monash2013) Trigger Particle", 1);
+
+    return ctl;
 }
