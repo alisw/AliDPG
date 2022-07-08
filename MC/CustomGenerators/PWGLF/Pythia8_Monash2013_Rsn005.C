4c79321308220d12483c770e8afd47f1c24ed1cd
Bong-Hwi Lim <bong-hwi.lim@cern.ch>
2022-07-08 14:07:47 +0200

    remove rho

--- MC/CustomGenerators/PWGLF/Pythia8_Monash2013_Rsn005.C
+++ MC/CustomGenerators/PWGLF/Pythia8_Monash2013_Rsn005.C
@@ -16,9 +16,8 @@ AliGenerator* GeneratorCustom()
   ctl->AddGenerator(py8, "Pythia8 (Monash2013)", 1.);
 
   // randomly injected particles
-  const int nParticles = 5;
-  particle_inj particleList[5] = { // {name,pdgcode,maxpt,maxy},
-    {1,"rho", 113, 15., 0.6},
+  const int nParticles = 4;
+  particle_inj particleList[4] = { // {name,pdgcode,maxpt,maxy},
     {1,"Sigma*+(1385)",3224,15.,0.6},
     {1,"Sigma*+_bar(1385)",-3224,15.,0.6},
     {1,"Sigma*-(1385)",3114,15.,0.6},
