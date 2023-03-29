2efaa84546da44934b311edfa74b327fb40ad5a9
jditzelnew <58816213+jditzelnew@users.noreply.github.com>
2023-03-29 09:48:03 +0200

    Added 4(Xi-)He

--- MC/CustomGenerators/PWGLF/Hijing_Nuclex006.C
+++ MC/CustomGenerators/PWGLF/Hijing_Nuclex006.C
@@ -5,10 +5,11 @@ GeneratorCustom()
   AliGenCocktail *ctl   = (AliGenCocktail*) GeneratorCocktail("Hijing_Nuclex006");
   AliGenerator   *hij   = GeneratorHijing();
   ctl->AddGenerator(hij,  "Hijing", 1.);
-  //	hypertriton, hyperhydrogen-4, hyperhelium-4, double-hyperhydrogen-4
-  AliGenerator   *nu1a  = Generator_Nuclex(0x10C010, kFALSE, 40);
-  AliGenerator   *nu1b  = Generator_Nuclex(0x10C010, kTRUE, 40);
+  //	hypertriton, hyperhydrogen-4, hyperhelium-4, double-hyperhydrogen-4, 4(Xi-)He
+  AliGenerator   *nu1a  = Generator_Nuclex((0x10 | 0x4000 | 0x8000 | 0x100000 | 0x200000), kFALSE, 40);
+  AliGenerator   *nu1b  = Generator_Nuclex((0x10 | 0x4000 | 0x8000 | 0x100000 | 0x200000), kTRUE,  40);
   ctl->AddGenerator(nu1a,  "Nuclex1a", 1.);
   ctl->AddGenerator(nu1b,  "Nuclex1b", 1.);
+
   return ctl;
 }
