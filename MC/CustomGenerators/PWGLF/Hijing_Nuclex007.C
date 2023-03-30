238dfc7831c1af46dfc742e90d402a1f726abe3f
jditzelnew <58816213+jditzelnew@users.noreply.github.com>
2023-03-30 09:04:12 +0200

    adding max_pt, max_y

--- MC/CustomGenerators/PWGLF/Hijing_Nuclex007.C
+++ MC/CustomGenerators/PWGLF/Hijing_Nuclex007.C
@@ -7,13 +7,13 @@ GeneratorCustom()
   ctl->AddGenerator(hij,  "Hijing", 1.);
   
   //	hypertriton, hyperhydrogen-4, hyperhelium-4, double-hyperhydrogen-4
-  AliGenerator   *nu1a  = Generator_Nuclex((0x10 | 0x4000 | 0x8000 | 0x100000 | 0x200000), kFALSE, 40);
-  AliGenerator   *nu1b  = Generator_Nuclex((0x10 | 0x4000 | 0x8000 | 0x100000 | 0x200000), kTRUE,  40);
+  AliGenerator   *nu1a  = Generator_Nuclex((0x10 | 0x4000 | 0x8000 | 0x100000 | 0x200000), kFALSE, 40, 10., 1.);
+  AliGenerator   *nu1b  = Generator_Nuclex((0x10 | 0x4000 | 0x8000 | 0x100000 | 0x200000), kTRUE,  40, 10., 1.);
   ctl->AddGenerator(nu1a,  "Nuclex1a", 1.);
   ctl->AddGenerator(nu1b,  "Nuclex1b", 1.);
   //	helium3, triton, alpha
-  AliGenerator   *nu2a  = Generator_Nuclex((0x2 | 0x4 | 0x8), kFALSE, 10);
-  AliGenerator   *nu2b  = Generator_Nuclex((0x2 | 0x4 | 0x8), kTRUE,  10);
+  AliGenerator   *nu2a  = Generator_Nuclex((0x2 | 0x4 | 0x8), kFALSE, 10, 10., 1.);
+  AliGenerator   *nu2b  = Generator_Nuclex((0x2 | 0x4 | 0x8), kTRUE,  10, 10., 1.);
   ctl->AddGenerator(nu2a,  "Nuclex2a", 1.);
   ctl->AddGenerator(nu2b,  "Nuclex2b", 1.);
   return ctl;
