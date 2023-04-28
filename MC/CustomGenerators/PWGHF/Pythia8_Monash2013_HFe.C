ad55b55d96c79a078e8bfa6efcd64597224d7da4
mvolkl <38325723+mvolkl@users.noreply.github.com>
2023-04-28 16:05:46 +0200

    Include additional pi0 and eta signals

--- MC/CustomGenerators/PWGHF/Pythia8_Monash2013_HFe.C
+++ MC/CustomGenerators/PWGHF/Pythia8_Monash2013_HFe.C
@@ -1,5 +1,7 @@
 AliGenerator *GeneratorCustom(TString opt = "")
 {
+  const Char_t *label[3] = {    "cele PYTHIA", "bele PYTHIA"  };
+  AliGenCocktail *ctl  = static_cast<AliGenCocktail *>(GeneratorCocktail("Monash2013HFe"));
 
   //Switches for prompt/nonprompt
   Process_t process[2] = {kPyCharmppMNRwmi, kPyBeautyppMNRwmi}; //charm or beauty
@@ -24,6 +26,36 @@ AliGenerator *GeneratorCustom(TString opt = "")
    
   // lambda_b from PDG 2019: tau0 = 1.471 ps = 441 m/c = 0.441 mm/c
   (AliPythia8::Instance())->ReadString("5122:tau0 = 4.41000e-01");
+
+  ctl->AddGenerator(pyth, label[iprocess], 1.);
+
+  AliGenPHOSlib *plib = new AliGenPHOSlib();
+  Double_t ptMaxInjected = 50.;
+  if(ptmaxConfig != -1){
+    ptMaxInjected = ptmaxConfig;
+  }
+
+  AliGenParam *pi0 = new AliGenParam(2, plib, AliGenPHOSlib::kPi0Flat);
+  pi0->SetPhiRange(0., 360.) ;
+  pi0->SetYRange(-1.2, 1.2) ;
+  pi0->SetPtRange(0., ptMaxInjected);
+  pi0->SetKeepIfOneChildSelected(kTRUE);
+  pi0->SetCutOnChild(1);
+  pi0->SetPdgCodeParticleforAcceptanceCut(11);
+  pi0->SetChildYRange(-1.2, 1.2);
+  pi0->SetChildPtRange(pttrigminConfig, 10000.);
+  ctl->AddGenerator(pi0,  "pi0", 1.);
+
+  AliGenParam *eta = new AliGenParam(1, plib, AliGenPHOSlib::kEtaFlat);
+  eta->SetPhiRange(0., 360.) ;
+  eta->SetYRange(-1.2, 1.2) ;
+  eta->SetPtRange(0., ptMaxInjected);
+  eta->SetKeepIfOneChildSelected(kTRUE);
+  eta->SetCutOnChild(1);
+  eta->SetPdgCodeParticleforAcceptanceCut(11);
+  eta->SetChildYRange(-1.2, 1.2);
+  eta->SetChildPtRange(pttrigminConfig, 10000.);
+  ctl->AddGenerator(eta,  "eta", 1.);
   
-  return pyth;
+  return ctl;
 }
