565a4ddeda17049c86a36aea7075033f7966a0cb
Stephan Friedrich Stiefelmaier <stephan.friedrich.stiefelmaier@cern.ch>
2023-12-13 18:17:02 +0100

    Update Hijing_Gamma002.C
    reduce pT range for PCM injectors and add EMCAL injectors

--- MC/CustomGenerators/PWGGA/Hijing_Gamma002.C
+++ MC/CustomGenerators/PWGGA/Hijing_Gamma002.C
@@ -26,15 +26,15 @@ GeneratorCustom()
   }
   
   // PCM
-  TFormula* neutralsF  = new TFormula("neutrals",  "max(1.,470.*(x<5.)+72.*(x>7.5)*(x<12.5))");
+  TFormula* neutralsF  = new TFormula("neutrals",  "max(1.,470.*(x<5.)+62.*(x>7.5)*(x<12.5))");
   Int_t ntimes = 1;
   if ( isEmbedding )
   {
     ntimes=neutralsF->Eval(bEvalFormula);
     delete neutralsF; neutralsF=0x0;
   }
-  AliGenerator   *pi0  = GeneratorInjector(1, 111, 0., 25., -1.2, 1.2);
-  AliGenerator   *eta  = GeneratorInjector(1, 221, 0., 25., -1.2, 1.2);
+  AliGenerator   *pi0  = GeneratorInjector(1, 111, 0., 10., -1.2, 1.2);
+  AliGenerator   *eta  = GeneratorInjector(1, 221, 0., 10., -1.2, 1.2);
   ctl->AddGenerator(pi0,  "Injector (pi0)", 1., neutralsF,ntimes);
   ctl->AddGenerator(eta,  "Injector (eta)", 1., neutralsF,ntimes);
   
@@ -49,6 +49,28 @@ GeneratorCustom()
   ctl->AddGenerator(pi0c, "Injector (pi0c)", 1.);
   ctl->AddGenerator(pi0d, "Injector (pi0d)", 1.);
   ctl->AddGenerator(etaa, "Injector (etaa)", 1.);
-  
+
+  // EMCAL
+  AliGenerator   *pi0e = GeneratorInjector(1, 111, 0., 25., -0.7, 0.7, 80., 120.); // EMCal 1
+  AliGenerator   *pi0f = GeneratorInjector(1, 111, 0., 25., -0.7, 0.7, 120., 160.); // EMCal 2
+  AliGenerator   *pi0g = GeneratorInjector(1, 111, 0., 25., -0.7, 0.7, 160., 187.); // EMCal 3
+  AliGenerator   *pi0h = GeneratorInjector(1, 111, 0., 25., 0.22, 0.7, 260., 327.); // DCal one side PHOS
+  AliGenerator   *pi0i = GeneratorInjector(1, 111, 0., 25., -0.7, -0.22, 260., 327.); // DCal other side PHOS
+  AliGenerator   *etab = GeneratorInjector(1, 221, 0., 25.,  -0.7, 0.7, 80., 120.); // EMCal 1
+  AliGenerator   *etac = GeneratorInjector(1, 221, 0., 25.,  -0.7, 0.7, 120., 160); // EMCal 2
+  AliGenerator   *etad = GeneratorInjector(1, 221, 0., 25.,  -0.7, 0.7, 160., 187.); // EMCal 3
+  AliGenerator   *etae = GeneratorInjector(1, 221, 0., 25.,  0.22, 0.7, 260., 327.); // DCal one side PHOS
+  AliGenerator   *etaf = GeneratorInjector(1, 221, 0., 25., -0.7, -0.22, 260., 327.); // DCal other side PHOS
+  ctl->AddGenerator(pi0e, "Injector (pi0e)", 1.);
+  ctl->AddGenerator(pi0f, "Injector (pi0f)", 1.);
+  ctl->AddGenerator(pi0g, "Injector (pi0g)", 1.);
+  ctl->AddGenerator(pi0h, "Injector (pi0h)", 1.);
+  ctl->AddGenerator(pi0i, "Injector (pi0i)", 1.);
+  ctl->AddGenerator(etab, "Injector (etab)", 1.);
+  ctl->AddGenerator(etac, "Injector (etac)", 1.);
+  ctl->AddGenerator(etad, "Injector (etad)", 1.);
+  ctl->AddGenerator(etae, "Injector (etae)", 1.);
+  ctl->AddGenerator(etaf, "Injector (etaf)", 1.);
+
   return ctl;
 }
